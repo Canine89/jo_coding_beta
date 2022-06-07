d97c8393f7455b3d22bace91b934deec4cc2e036
canine89 <phk707kr@gmail.com>
2022-06-07 18:03:31 +0900

    240쪽까지 실습 완료. 예외 처리로 SSL 오류는 사라졌습니다.

--- app.py
+++ app.py
@@ -2,22 +2,32 @@ from selenium import webdriver
 import time
 import urllib.request
 
+search = "고양이"
+fileName = "cat_image"
+number = 30
+interval = 0.2
+
 driver = webdriver.Chrome("./chromedriver")
 driver.get(
-    "https://www.google.com/search?q=고양이&tbm=isch&hl=ko&tbs=il:ol&sa=X&ved=0CAAQ1vwEahcKEwigpLGVq_7xAhUAAAAAHQAAAAAQAg&biw=1126&bih=682"
+    f"https://www.google.com/search?q={search}&tbm=isch&hl=ko&tbs=il:ol&sa=X&ved=0CAAQ1vwEahcKEwigpLGVq_7xAhUAAAAAHQAAAAAQAg&biw=1126&bih=682"
 )
 firstImage = driver.find_element_by_css_selector(
     "#islrg > div.islrc > div:nth-child(2) > a.wXeWr.islib.nfEiy > div.bRMDJf.islir > img"
 )
 firstImage.click()
 
-time.sleep(0.2)
+for i in range(number):
+    try:
+        time.sleep(interval)
+        image = driver.find_element_by_css_selector("#Sva75c > div > div > div.pxAole > div.tvh9oe.BIB1wf > c-wiz > div > div.OUZ5W > div.zjoqD > div.qdnLaf.isv-id.b0vFpe > div > a > img")
+        imageSrc = image.get_attribute("src")
+        urllib.request.urlretrieve(imageSrc, f"{fileName}_{i+1}.jpg")
+    except:
+        print(f"{i+1}번째 이미지 저장 오류 발생!")
+    else:
+        print(f"{i+1}번째 이미지 저장 성공!")
+    finally:
+        nextButton = driver.find_element_by_css_selector("#Sva75c > div > div > div.pxAole > div.tvh9oe.BIB1wf > c-wiz > div > div.OUZ5W > div.zjoqD > div.mWagE.fDqwl > a:nth-child(4)")
+        nextButton.click()
 
-image = driver.find_element_by_css_selector(
-    "#Sva75c > div > div > div.pxAole > div.tvh9oe.BIB1wf > c-wiz > div > div.OUZ5W > div.zjoqD > div.qdnLaf.isv-id.b0vFpe > div > a > img"
-)
-imageSrc = image.get_attribute("src")
-urllib.request.urlretrieve(imageSrc, "cat_image.jpg")
-
-time.sleep(10)
 driver.quit()
